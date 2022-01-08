d6f7f6a3eff5752c5f5c26eae47fd88e5c484b0f
Polpy <emailparaoedu@gmail.com>
2022-01-08 15:22:01 +0000

    update reset

--- Codigo_outro_RTC/Codigo_outro_RTC.ino
+++ Codigo_outro_RTC/Codigo_outro_RTC.ino
@@ -1,3 +1,4 @@
+   
 #include "ThreeWire.h" //INCLUSÃO DA BIBLIOTECA
 #include "RtcDS1302.h" //INCLUSÃO DA BIBLIOTECA
 #include <MD_MAX72xx.h>
@@ -9,23 +10,25 @@
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 8 //mudar isto para testar metade.
 
-#define CLK_PIN_0   4 // 13  // or SCK
-#define DATA_PIN_0  3 // 11  // or MOSI
-#define CS_PIN_0    2 // 10  // or SS
+#define CLK_PIN_0   7   // or SCK
+#define DATA_PIN_0  5   // or MOSI
+#define CS_PIN_0    6  // or SS
 
-#define CLK_PIN_1   7
-#define DATA_PIN_1  6
-#define CS_PIN_1    5
+#define CLK_PIN_1   10
+#define DATA_PIN_1  8
+#define CS_PIN_1    9
 
-#define CLK_PIN_2  10
-#define DATA_PIN_2  9
-#define CS_PIN_2    8
+#define CLK_PIN_2  13
+#define DATA_PIN_2  11
+#define CS_PIN_2    12
 
 #define SPEED_TIME  80
 #define PAUSE_TIME  10
 #define MAX_MESG  20
 
-ThreeWire myWire(12, 13, 11); //OBJETO DO TIPO ThreeWire
+#define Reset A0 //to reset the Arduino
+
+ThreeWire myWire(3, 4, 2); // DAT, CLK, RST
 RtcDS1302<ThreeWire> Rtc(myWire); //OBJETO DO TIPO RtcDS1302
 
 MD_Parola row0 = MD_Parola(HARDWARE_TYPE, DATA_PIN_0, CLK_PIN_0, CS_PIN_0, MAX_DEVICES);
@@ -34,14 +37,19 @@ MD_Parola row2 = MD_Parola(HARDWARE_TYPE, DATA_PIN_2, CLK_PIN_2, CS_PIN_2, MAX_D
 
 // Global message buffers shared by Serial and Scrolling functions
 #define BUF_SIZE  75
+
 char strTIME[BUF_SIZE]= "00:00:00";
 //char strTIMEsmall1[BUF_SIZE]= "00 HOURS";
 //char strTIMEsmall2[BUF_SIZE]= "00 MINUTES 00 DAYS";
-char strDATE[BUF_SIZE]= "0 YEARS 0 DAYS";
+char strDATE[BUF_SIZE]= "0Y 0 DAYS";
 
 void setup()
 {
+  digitalWrite(Reset, HIGH);
+  pinMode(Reset, OUTPUT);
   Serial.begin(57600);
+  
+
   
   row0.begin();
   row1.begin();
@@ -60,9 +68,16 @@ void setup()
   //row1.displayText(strTIMEsmall1, PA_CENTER, SPEED_TIME, PAUSE_TIME, PA_PRINT, PA_NO_EFFECT);
   //row0.displayText(strTIMEsmall2, PA_CENTER, SPEED_TIME, PAUSE_TIME, PA_PRINT, PA_NO_EFFECT);
 
+
+  Serial.print("compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+  
   Rtc.Begin(); //INICIALIZA O RTC
   RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__); //VARIÁVEL RECEBE DATA E HORA DE COMPILAÇÃO
-
+  //printDateTime(compiled);
+  //Serial.println();
+  
   if(Rtc.GetIsWriteProtected()){ //SE O RTC ESTIVER PROTEGIDO CONTRA GRAVAÇÃO, FAZ
         Serial.println("RTC está protegido contra gravação. Habilitando a gravação agora..."); //IMPRIME O TEXTO NO MONITOR SERIAL
         Rtc.SetIsWriteProtected(false); //HABILITA GRAVAÇÃO NO RTC
@@ -70,7 +85,12 @@ void setup()
     }
     
   if (!Rtc.GetIsRunning())
+  {
+    Serial.println("rtc not running");
     Rtc.SetIsRunning(true); //INICIALIZA O RTC
+    
+  }
+    
     
   RtcDateTime now = Rtc.GetDateTime(); //VARIÁVEL RECEBE INFORMAÇÕES
    if (now < compiled) { //SE A INFORMAÇÃO REGISTRADA FOR MENOR QUE A INFORMAÇÃO COMPILADA, FAZ
@@ -78,6 +98,14 @@ void setup()
         Rtc.SetDateTime(compiled); //INFORMAÇÕES COMPILADAS SUBSTITUEM AS INFORMAÇÕES ANTERIORES
         Serial.println(); //QUEBRA DE LINHA NA SERIAL
   }
+   else if (now > compiled) 
+    {
+        Serial.println("RTC is newer than compile time. (this is expected)");
+    }
+    else if (now == compiled) 
+    {
+        Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+    }
 }
 
 
@@ -90,29 +118,30 @@ struct date {
 //http://prntscr.com/13331o6 (Em 18:28 18/05/2021)
 
 struct date limitdate = {
-  .year = 2029, 
+  .year = 2030, 
   .month = 7,
   .day = 27,
-  .hour = 20,
-  .minute = 00, 
-  .sec = 00,
+  .hour = 24,
+  .minute = 60, 
+  .sec = 60,
 };
 
+
 #define countof(a) (sizeof(a) / sizeof(a[0]))
-  
+
 void loop()
 {
   static uint32_t lastTime = 0; // millis() memory
   
-  row0.displayAnimate();
-  row1.displayAnimate();
-  row2.displayAnimate();
   
-  
-  
+
   // Finally, adjust the time string if we have to
   if (millis() - lastTime >= 1000)
   {
+    row0.displayAnimate();
+    row1.displayAnimate();
+    row2.displayAnimate();
+    
     RtcDateTime now = Rtc.GetDateTime(); //VARIÁVEL RECEBE INFORMAÇÕES
     struct date currentdate = {
     .year = now.Year(),
@@ -122,54 +151,41 @@ void loop()
     .minute = now.Minute(), 
     .sec = now.Second(),
     };
-
-    // printDateTime(now); Print Date and Time now
+    //printDateTime(now);
     
     lastTime = millis();
   
     YEARSLEFT = abs(limitdate.year - currentdate.year-1);
     DAYSLEFT = abs(dateDiff(currentdate.year,currentdate.month,currentdate.day,limitdate.year,limitdate.month,limitdate.day));  
-    HRSLEFT = abs(24- limitdate.hour - currentdate.hour-1)%24;
-    MINSLEFT = abs(60-limitdate.minute - currentdate.minute-1)%60;
-    SECSLEFT = abs(60-limitdate.sec - currentdate.sec-1)%60;
-
-
-    //sprintf(strTIMEsmall1, "%02d HOURS",HRSLEFT%24);
-    //sprintf(strTIMEsmall2, "%02d M %02d S",MINSLEFT,SECSLEFT);
-
-    sprintf(strTIME, "%02d:%02d:%02d",HRSLEFT%24,MINSLEFT,SECSLEFT); //grande.
+    HRSLEFT = abs(limitdate.hour - currentdate.hour-1)%24;
+    MINSLEFT = abs(limitdate.minute - currentdate.minute-1)%60;
+    SECSLEFT = abs(limitdate.sec - currentdate.sec-1)%60;
+   
     
-    char debug[200];
-    sprintf(debug, "y: %d, d: %d", YEARSLEFT, DAYSLEFT);
+    char currentdatestrbug[200];
+    sprintf(currentdatestrbug, "TRUED{%02d:%02d:%02d %d day %d month %d year}", currentdate.hour, currentdate.minute,currentdate.sec,currentdate.day,currentdate.month, currentdate.year);
+    //Serial.println(currentdatestrbug);
     
-    Serial.println(debug);
-    Serial.println(strTIME);
-    
-    //sprintf(strDATE, "%d YEARS %d DAYS",YEARSLEFT,DAYSLEFT%366); //grande.
+    sprintf(strTIME, "%02d:%02d:%02d",HRSLEFT,MINSLEFT,SECSLEFT); //grande.
     sprintf(strDATE, "%dY %d DAYS",YEARSLEFT,DAYSLEFT%366);
+    //Serial.print(strDATE);
+    //Serial.println(strTIME);
+    
+    //sprintf(strTIMEsmall1, "%02d HOURS",HRSLEFT%24);
+    //sprintf(strTIMEsmall2, "%02d M %02d S",MINSLEFT,SECSLEFT);    
+    //sprintf(strDATE, "%d YEARS %d DAYS",YEARSLEFT,DAYSLEFT%366); //grande.
+    
     row0.displayReset();
     row1.displayReset();
     row2.displayReset();
-  }
-  if(HOURSLEFT==1){
-    row0.begin();
-    row1.begin();
-    row2.begin();
-  
-    row0.setFont(BigFontLower); //grande
-    row1.setFont(BigFontUpper); //grande
-    //row0.setFont(NULL);
-    //row1.setFont(NULL);
-    row2.setFont(NULL);
-    
-    
-    row2.displayText(strDATE, PA_CENTER, SPEED_TIME, PAUSE_TIME, PA_PRINT, PA_NO_EFFECT);
-    row1.displayText(strTIME, PA_CENTER, SPEED_TIME, PAUSE_TIME, PA_PRINT, PA_NO_EFFECT); //grande
-    row0.displayText(strTIME, PA_CENTER, SPEED_TIME, PAUSE_TIME, PA_PRINT, PA_NO_EFFECT); //grande
+    if(HRSLEFT+1%2==0 && MINSLEFT==12 && SECSLEFT==32)
+    {
+     digitalWrite(Reset, LOW);
+    }
   }
 }
 
-void printDateTime(const RtcDateTime& dt){
+/*void printDateTime(const RtcDateTime& dt){
     char datestring[20]; //VARIÁVEL ARMAZENA AS INFORMAÇÕES DE DATA E HORA
  
     snprintf_P( datestring, 
@@ -183,7 +199,7 @@ void printDateTime(const RtcDateTime& dt){
                 dt.Second()
             ); //SEGUNDOS
     Serial.println(datestring); //IMPRIME NO MONITOR SERIAL AS INFORMAÇÕES
-}
+}*/
 
 int dateDiff(int year1, int mon1, int day1, int year2, int mon2, int day2)
 {
